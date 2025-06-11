fca2c0af625a62e60db932340c2598e9d9e231f3
Ebith Lawrence <ebith.lawrence@gmail.com>
2025-06-11 13:21:15 +0530

    Debug vital updates

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ const App = () => {
 
   useEffect(() => {
     //const socket = io("http://localhost:5000");
-    const socket = io(`${backendUrl}`, {transports: ["websocket"], withCredentials: true});
+    //const socket = io(`${backendUrl}`, {transports: ["websocket"], withCredentials: true});
+    const socket = io(`${backendUrl}`, {transports: ["websocket", "polling"], reconnectionAttempts: 5, reconnectionDelay: 2000});
+
     //const socket = io(`${backendUrl}`);
     
     socket.on("connect", () => {
